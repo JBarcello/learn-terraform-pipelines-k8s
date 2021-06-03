3c16774e0b6ef931060c2b528311b4838ad65cb9
JBarcello <78364005+JBarcello@users.noreply.github.com>
2021-06-03 10:05:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"    
+    organization = "infrastructure-pipelines-workshop"    
     workspaces {
-      name = "learn-terraform-pipelines-k8s"
+      name = "jeremy-b-k8s"
     }
   }
 required_providers {
